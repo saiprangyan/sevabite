8a59652b605ed26160382b8b1e1146028dedc882
saiprangyan <saiprangyanpriyadarshini@gmail.com>
2026-04-12 11:33:55 +0530

    remove unused useCountUp function

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -4,20 +4,7 @@ import { motion } from 'framer-motion';
 import axios from 'axios';
 import API_URL from '../config';
 
-function useCountUp(target, duration = 2000) {
-  const [count, setCount] = useState(0);
-  useEffect(() => {
-    let start = 0;
-    const step = target / (duration / 16);
-    const timer = setInterval(() => {
-      start += step;
-      if (start >= target) { setCount(target); clearInterval(timer); }
-      else setCount(Math.floor(start));
-    }, 16);
-    return () => clearInterval(timer);
-  }, [target, duration]);
-  return count;
-}
+
 
 
 function Home() {
